613428eb5366c74aa8135d66ebb3cd01c8ced249
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-02 21:20:59 +0545

    Update practiceAll.js

--- WebDeveloperIntern/DSA-Interview-Qns/practiceAll.js
+++ WebDeveloperIntern/DSA-Interview-Qns/practiceAll.js
@@ -1,5 +1,5 @@
 
-// all this question need to read and understand first and then research about it
+// all this question need to read and understand first and then research about it to mastering
 //Javascript Interview Qna
 // Basic JavaScript Questions with Answers
 
